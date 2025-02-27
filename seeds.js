ac3576711328401dceb23678278b8f3b352fa0d7
Swagatam <swagatamheroz@outlook.com>
2025-02-27 19:47:15 +0530

    fix: remote db connection

--- seeds.js
+++ seeds.js
@@ -5,11 +5,11 @@
 import "dotenv/config";
 import pg from "pg";
 
+const { native } = pg;
+const { Client } = native;
+
 const SQL = `
-CREATE TABLE IF NOT EXISTS examples (
-  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  example_text VARCHAR ( 255 )
-);
+DELETE FROM examples;
 
 INSERT INTO examples (example_text)
 VALUES
@@ -21,8 +21,11 @@ VALUES
 async function main() {
   console.log("seeding started ...");
 
-  const client = new pg.Client({
+  const client = new Client({
     connectionString: process.env.DB_CONNECTION_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    }
   });
 
   await client.connect();
